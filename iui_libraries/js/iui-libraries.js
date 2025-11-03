ef6e0303ab9d1bcc3ff8cb0336395b27b16b8125
Andy Smith <andjsmit@iupui.edu>
2025-11-03 15:50:32 -0500

    Drush generate theme using Stable9

---
+++ iui_libraries/js/iui-libraries.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * IU Indianapolis Libraries behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.iuiLibraries = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
